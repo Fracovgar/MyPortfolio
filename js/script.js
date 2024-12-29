9ac114dd1e9344d182c5461c2fe21a9987d37bf3
Francisco Coves <fracovgar@gmail.com>
2024-12-29 05:34:51 +0100

    Avances en la sección sobremi

--- js/script.js
+++ js/script.js
@@ -12,6 +12,22 @@ const menuItems = document.querySelectorAll(".side-nav nav ul li");
 // Elemento de sonido
 const pageSound = document.getElementById("page-sound");
 
+//Cargar el Curriculum
+const previewButton = document.getElementById('preview');
+
+previewButton.addEventListener('click', () => {
+    const pdfUrl = './cv/FranciscoCoves_CV.pdf';
+    const popupWidth = 800;
+    const popupHeight = 800;
+
+    // Coordenadas para centrar la ventana en el área visible del navegador
+    const left = (window.innerWidth - popupWidth) / 2;
+    const top = (window.innerHeight - popupHeight) / 2;
+
+    // Abrir la ventana centrada
+    window.open(pdfUrl, '_blank', `width=${popupWidth},height=${popupHeight},scrollbars=yes,left=${left},top=${top}`);
+});
+
 // Configuración inicial
 let currentIndex = 0; 
 const keys = Object.keys(secciones); 
@@ -51,4 +67,4 @@ function moverPagina(targetIndex) {
 function activarMenu(targetIndex) {
     menuItems.forEach(item => item.classList.remove("active"));
     menuItems[targetIndex].classList.add("active");
-}º
+}
